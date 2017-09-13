38fc3a7670daf4eb28002e502e14c8e970cc5370
kazakov-ab <e@mail.com>
2017-09-13 16:04:11 +0300

    simplify connection test

---
+++ src/main/java/test/connection/PoolingDataSourceTest.java
@@ -0,0 +1,24 @@
+package test.connection;
+
+import org.junit.Test;
+import test.connection.readonly.impl.Connection;
+import test.connection.readonly.impl.DataSource;
+import test.connection.readonly.impl.EchoDataSource;
+
+import static junit.framework.Assert.assertEquals;
+
+public class PoolingDataSourceTest {
+    private DataSource ds = new PoolingDataSource(new EchoDataSource());
+
+    @Test
+    public void reuseConnection() throws Exception {
+        assertEquals("test1", executeQuery("test1"));
+        assertEquals("test2", executeQuery("test2"));
+    }
+
+    private String executeQuery(String query) {
+        try (Connection c = ds.getConnection()) {
+            return String.valueOf(c.query(query));
+        }
+    }
+}
