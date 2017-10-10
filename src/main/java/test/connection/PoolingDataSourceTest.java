bdb4a6f4eea6207a9d50f2e435130c7d38c2ce00
kazakov-ab <e@mail.com>
2017-10-10 13:34:03 +0300

    PoolingDataSourceTest

--- src/main/java/test/connection/PoolingDataSourceTest.java
+++ src/main/java/test/connection/PoolingDataSourceTest.java
@@ -6,21 +6,20 @@ import test.connection.readonly.impl.DataSource;
 import test.connection.readonly.impl.EchoDataSource;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertSame;
 
 public class PoolingDataSourceTest {
     private DataSource ds = new PoolingDataSource(new EchoDataSource());
 
     @Test
     public void reuseConnection() throws Exception {
-        assertEquals("test1", executeQuery("test1"));
-        assertEquals("test2", executeQuery("test2"));
-    }
+        Connection c1 = ds.getConnection();
+        assertEquals("test1", c1.query("test1"));
+        c1.close();
 
-    private String executeQuery(String query) {
-        String res = null;
-        try (Connection c = ds.getConnection()) {
-            res = String.valueOf(c.query(query));
-        }
-        return res;
+        Connection c2 = ds.getConnection();
+        assertEquals("test2", c2.query("test"));
+        assertSame(c1, c2);
+        c2.close();
     }
 }
