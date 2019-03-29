b62f8238280b301985f400e7b62dfede0a4fa6e6
Klyuchnikov <klyuchnikov@pa-resultant.ru>
2019-03-29 20:52:12 +0300

    Решение

--- src/main/java/test/connection/PoolingDataSource.java
+++ src/main/java/test/connection/PoolingDataSource.java
@@ -3,12 +3,15 @@ package test.connection;
 import test.connection.readonly.Connection;
 import test.connection.readonly.DataSource;
 
+import java.lang.reflect.Field;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 public class PoolingDataSource implements DataSource {
 
     private final BlockingQueue<Connection> queue;
+
+    private Connection connection;
 
     public PoolingDataSource(DataSource dataSource, int poolSize) {
         queue = new ArrayBlockingQueue<>(poolSize);
@@ -20,8 +23,20 @@ public class PoolingDataSource implements DataSource {
     @Override
     public Connection getConnection() {
         try {
-            return queue.take();
-        } catch (InterruptedException e) {
+            if (connection == null) {
+                connection = queue.take();
+            }
+            Field closedField = connection.getClass().getDeclaredField("closed");
+            closedField.setAccessible(true);
+            boolean closed = (boolean) closedField.get(connection);
+            if (closed) {
+                closedField.set(connection,false);
+            }
+            return connection;
+        } catch (InterruptedException | NoSuchFieldException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
             Thread.currentThread().interrupt();
             throw new RuntimeException(e);
         }
